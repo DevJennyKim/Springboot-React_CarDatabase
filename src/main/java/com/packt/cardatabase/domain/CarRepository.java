0c912887d30fb1ca4c3a5ed5c206efb8957c5e98
Haeun[Jenny] Kim <dev.jennyK@gmail.com>
2024-12-17 13:13:08 -0700

    Setup Database

--- src/main/java/com/packt/cardatabase/domain/CarRepository.java
+++ src/main/java/com/packt/cardatabase/domain/CarRepository.java
@@ -1,5 +1,23 @@
 package com.packt.cardatabase.domain;
 
-public class CarRepository {
+import java.util.List;
 
+import org.springframework.data.repository.CrudRepository;
+
+public interface CarRepository extends CrudRepository<Car, Long> {
+	//Search by brand
+	List<Car> findByBrand(String brand);
+	//Search by color
+	List<Car> findByColor(String color);
+	//Search by year
+	List<Car> findByYear(int year);
+	
+	//Search by brand and model
+	List<Car> findByBrandAndModel(String brand, String model);
+	//Search by brand or color
+	List<Car> findByBrandOrColor(String brand, String color);
+	
+	//Search by brand and order by year
+	List<Car> findByBrandOrderByYearAsc(String brand);
+	
 }
